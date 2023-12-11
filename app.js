0c2cffc3944f77a6cd1f0b246cc8f65160bcaa84
royalmlip <130339883+royalmlip@users.noreply.github.com>
2023-12-11 11:21:14 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+// 引入 createVLESSServer 函数
+const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
+// 定义端口和 UUID
 const port = process.env.PORT || 3001;
+const uuid = process.env.UUID || "d342d11e-d424-4583-b36e-524ab1f0afa4";
+
+// 调用函数启动 VLESS 服务器
+createVLESSServer(port, uuid);
+
 
 app.get("/", (req, res) => res.type('html').send(html));
 
