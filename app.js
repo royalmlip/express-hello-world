ea8efb079899e7c1bbaab579aacd5d16cb675d39
royalmlip <130339883+royalmlip@users.noreply.github.com>
2023-12-26 13:05:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -17,33 +17,10 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 //以下是web.js模块的路由重写
 app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
-  '/' + wspath + '*',
-  createProxyMiddleware({
-    target: 'http://127.0.0.1:'+vlessPort+'/', /* 需要跨域处理的请求地址 */
-    ws: true, /* 是否代理websocket */
-    changeOrigin: true, /* 是否需要改变原始主机头为目标URL,默认false */ 
-    on: {  /* http代理事件集 */ 
-      proxyRes: function proxyRes(proxyRes, req, res) { /* 处理代理请求 */
-        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
-        // console.log(req) //for debug
-        // console.log(res) //for debug
-      },
-      proxyReq: function proxyReq(proxyReq, req, res) { /* 处理代理响应 */
-        // console.log(proxyReq); //for debug
-        // console.log(req) //for debug
-        // console.log(res) //for debug
-      },
-      error: function error(err, req, res) { /* 处理异常  */
-        console.warn('websocket error.', err);
-      }
-    },
-    pathRewrite: {
-      '^/': '/', /* 去除请求中的斜线号  */
-    },
-    // logger: console /* 是否打开log日志  */
-  })
+  '/' + wspath + '*',	/*代理websocket */
+  createVLESSServer(vlessPort, uuid)
 );
-createVLESSServer(vlessPort, uuid);
+
 
 const expressServer = app.listen(Port, () => console.log(`Express app listening on port ${Port}!`));
 
